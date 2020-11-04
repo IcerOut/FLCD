9e6c8340c997edd766f4e6070f713b117f16f769
Alin Lung <lung.s.alin@gmail.com>
2020-11-04 17:13:18 +0200

    Added examples

--- FiniteAutomata/FA main.py
+++ FiniteAutomata/FA main.py
@@ -2,5 +2,10 @@ from FiniteAutomata import FiniteAutomata
 
 if __name__ == '__main__':
     FA = FiniteAutomata.read_from_file('FA.in')
+    print(FA.sequence_accepted('0'))
+    print(FA.sequence_accepted('01'))
+    print(FA.sequence_accepted('01110'))
+    print(FA.sequence_accepted('111100'))
+    print(FA.sequence_accepted('011000101110'))
     while (res := FA.run_menu_once()) != '':
         print(res, '\n')
